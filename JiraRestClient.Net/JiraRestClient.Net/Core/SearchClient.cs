33a4ccaaa54a2a52abf57fffdd88e99b0576311b
Aleksandr Batalov <abataloff88@gmail.com>
2020-09-15 22:13:16 +0300

    Added startAt and maxResults parameters to search

--- JiraRestClient.Net/JiraRestClient.Net/Core/SearchClient.cs
+++ JiraRestClient.Net/JiraRestClient.Net/Core/SearchClient.cs
@@ -16,6 +16,8 @@ namespace JiraRestClient.Net.Core
         {
             var uri = UriHelper.BuildPath(BaseUri, RestPathConstants.SEARCH);
             uri = UriHelper.AddQuery(uri, "jql",jqlSearchBean.Jql);
+            uri = UriHelper.AddQuery(uri,"startAt", jqlSearchBean.StartAt.ToString());
+            uri = UriHelper.AddQuery(uri,"maxResults", jqlSearchBean.MaxResults.ToString());
             var response = Client.GetAsync(uri.ToString());
             var readAsStringAsync = response.Result.Content.ReadAsStringAsync();
             var result = readAsStringAsync.Result;
